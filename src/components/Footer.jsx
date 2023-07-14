396490807548388e24e78ddeb6b131043d578fd5
TadeoCuchman <tadeo.cuchman@gmail.com>
2023-07-14 16:46:24 -0300

    footer improvement

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ import twitterLogo from "../assets/twitterlogo.svg"
 const Footer = () => {
   return (
     <footer>
-        <div><img className="logo" src ={instaLogo} alt="instagram logo"/></div>
-        <div>m.braselli@gmail.com</div>
+        <a href="https://www.instagram.com/bbrasa/" target="_blank"><img className="logo" src ={instaLogo} alt="instagram logo"/></a>
+        <a style={{color:'black'}} href="mailto:m.braselli@gmail.com">m.braselli@gmail.com</a>
   </footer>
   )
 };
